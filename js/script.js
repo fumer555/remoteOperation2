0effbdc7aa607c13df57bef2f159ace32712e43c
fumer555 <47259041+fumer555@users.noreply.github.com>
2021-03-02 20:52:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var thisComment = [
 {"name": "fumer555","date": "14 Jan, 2020", "body": "Hello World! 4"}
 ];
 
+console.log(thisComment);
+
 for(var i=0;i<thisComment.length;i++){
 	var html = "<div class='commentBox'><div class='leftPanelImg'><img src='images/profile.png'></div><div class='rightPanel'><span>"+thisComment[i].name+"</span><div class='date'>"+thisComment[i].date+"</div><p>"+thisComment[i].body+"</p></div><div class='clear'></div></div>";
 	$('#container').append(html);
